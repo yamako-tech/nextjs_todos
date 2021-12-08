ae9c0c9a8e0710687fdbf28ababc65ab38fcf081
yamakon <askkyodai@gmail.com>
2021-12-08 19:24:30 +0900

    Login page: Tailwind template

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 
 export default function Layout({ children, title = "Default title"}) {
     return (
-        <div className="flex justify-center flex-col min-h-screen text-white font-mono bg-green-900">
+        <div className="flex justify-center flex-col min-h-screen text-blue-800 font-mono bg-indigo-50">
             <Head>
                 <title>{title}</title>
             </Head>
             <main className="flex flex-1 justify-center items-center w-screen flex-col">
                 {children}
             </main>
-            <footer className="w-full h-6 flex justify-center items-center text-yellow-300 text-sm">
+            <footer className="w-full h-6 flex justify-center items-center text-blue-800 text-sm">
                 @asktech 2021
             </footer>
         </div>
