5f86d51fe72637619ba50a3e2ebf9d984ee3244f
yamakon <askkyodai@gmail.com>
2021-12-09 09:52:17 +0900

    Auth login function

--- components/Auth.js
+++ components/Auth.js
@@ -38,28 +38,71 @@ export default function Auth() {
     } catch (err) {
       alert(err)
     }
+  };
+
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+        method: "POST",
+        body: JSON.stringify({ username: username, password: password}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => {
+        if (res.status === 400) {
+          throw "authentication failed";
+        }
+      });
+      login(); } catch (err){
+        alert(err);
+      }
+    }
   }
 
     return <div className="max-w-md w-full space-y-8">
     <div>
       <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-        Sign in to your account
+        {isLogin ? "Login" : "Sign up"}
       </h2>
     </div>
-    <form className="mt-8 space-y-6" action="#" method="POST">
+    <form className="mt-8 space-y-6" onSubmit={authUser}>
       <input type="hidden" name="remember" value="true" />
       <div className="rounded-md shadow-sm -space-y-px">
         <div>
-          <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+          <input 
+          name="username" 
+          type="text" 
+          autoComplete="username" 
+          required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+          placeholder="Username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+           />
         </div>
         <div>
-          <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+          <input 
+          name="password" 
+          type="password" 
+          autoComplete="current-password" 
+          required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+           />
         </div>
       </div>
       <div className="flex items-center justify-center">
         <div className="text-sm">
-          <span className="cursor-pointer font-medium text-indigo-600 hover:text-indigo-500">
+          <span onClick={() => setIsLogin(!isLogin)} className="cursor-pointer font-medium text-indigo-600 hover:text-indigo-500">
             Change mode?
           </span>
         </div>
@@ -71,7 +114,7 @@ export default function Auth() {
               <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
             </svg>
           </span>
-          Sign in
+          {isLogin ? "Login with JWT" : "Create new user"}
         </button>
       </div>
     </form>
