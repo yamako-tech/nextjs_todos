a39aa9cc7d52ddc42e10d98f321762420361ce14
yamakon <askkyodai@gmail.com>
2021-12-10 09:54:36 +0900

    Add link to post-detail

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,15 @@
+import Link from "next/link"
+
 export default function Post({ post }) {
     return (
         <div>
             <span>{post.id}</span>
             {" : "}
+            <Link href={`/posts/${post.id}`}>
             <span className="cursor-pointer text-indigo-700 border-b border-pink-200 hover:bg-blue-400 ">
                 {post.title}
             </span>
+            </Link>
         </div>
     )
 }
